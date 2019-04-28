526fce3e9658f1910d5833e448ab8067e027a069
wotjd243 <wotjd243@naver.com>
2019-04-28 10:25:24 +0900

    initialize the project

---
+++ src/main/java/io/github/wotjd243/shoppinggogo/ShoppingGoGoApplication.java
@@ -0,0 +1,11 @@
+package io.github.wotjd243.shoppinggogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingGoGoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingGoGoApplication.class, args);
+    }
+}
