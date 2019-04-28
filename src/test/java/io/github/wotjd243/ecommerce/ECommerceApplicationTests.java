c59e4a8041453b861a73ce6c397b227d4d7b55fb
wotjd243 <wotjd243@naver.com>
2019-04-28 10:30:03 +0900

    initialize the project

---
+++ src/test/java/io/github/wotjd243/ecommerce/ECommerceApplicationTests.java
@@ -0,0 +1,14 @@
+package io.github.wotjd243.ecommerce;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ECommerceApplicationTests {
+    @Test
+    public void contextLoads() {
+    }
+}
