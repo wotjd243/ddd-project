54e3f9c3e25ee3a2114af60a3f1716d5d9660a96
wotjd243 <wotjd243@naver.com>
2019-04-28 10:48:26 +0900

    initialize the project

---
+++ src/main/java/io/github/wotjd243/aladin/AladinApplication.java
@@ -0,0 +1,11 @@
+package io.github.wotjd243.aladin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AladinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AladinApplication.class, args);
+    }
+}
---
+++ src/test/java/io/github/wotjd243/aladin/AladinApplicationTests.java
@@ -0,0 +1,14 @@
+package io.github.wotjd243.aladin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AladinApplicationTests {
+    @Test
+    public void contextLoads() {
+    }
+}
