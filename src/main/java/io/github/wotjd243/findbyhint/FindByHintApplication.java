8bf244891837ed9be03a3fdc59374c772fe03cc2
wotjd243 <wotjd243@naver.com>
2019-04-28 10:16:14 +0900

    initialize the project

---
+++ src/main/java/io/github/wotjd243/findbyhint/FindByHintApplication.java
@@ -0,0 +1,11 @@
+package io.github.wotjd243.findbyhint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindByHintApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FindByHintApplication.class, args);
+    }
+}
---
+++ src/test/java/io/github/wotjd243/findbyhint/FindByHintApplicationTests.java
@@ -0,0 +1,14 @@
+package io.github.wotjd243.findbyhint;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FindByHintApplicationTests {
+	@Test
+	public void contextLoads() {
+	}
+}
