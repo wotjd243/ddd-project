e620895bf7152b1ee1b7d2a2dadd8739c654ff9a
wotjd243 <wotjd243@naver.com>
2019-04-28 10:33:02 +0900

    initialize the project

---
+++ src/main/java/io/github/wotjd243/book/BookApplication.java
@@ -0,0 +1,11 @@
+package io.github.wotjd243.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookApplication.class, args);
+    }
+}
---
+++ src/test/java/io/github/wotjd243/book/BookApplicationTests.java
@@ -0,0 +1,14 @@
+package io.github.wotjd243.book;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookApplicationTests {
+    @Test
+    public void contextLoads() {
+    }
+}
