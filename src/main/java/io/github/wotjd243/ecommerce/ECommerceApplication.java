c9eff77ad67437d5c54cc6833b91a2fb17163107
kbh1 <yesyesyes@etomato.com>
2019-04-29 11:12:50 +0900

    commit test

---
+++ src/main/java/io/github/wotjd243/ecommerce/ECommerceApplication.java
@@ -0,0 +1,11 @@
+package io.github.wotjd243.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceApplication.class, args);
+    }
+}
