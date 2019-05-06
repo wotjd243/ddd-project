1f60530a7ad47bae3df69f4b26de3a0dcd9654be
tramyu <tramyu@naver.com>
2019-05-06 20:20:23 +0900

    feat(domain) : domain 레이어 구현
    - Value Object 추가 및 구조 변경, 테스트 코드 작성

---
+++ book-core/src/test/java/io/github/book/domain/collected/domain/BookRentStatusTest.java
@@ -0,0 +1,23 @@
+package io.github.book.domain.collected.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("BookRentStatus enum 테스트")
+class BookRentStatusTest {
+
+    @Test
+    @DisplayName("렌트가 가능한 상태 여부 테스트")
+    void testRentAvailable() {
+        assertThat(BookRentStatus.RENT_AVAILABLE.isRent()).isTrue();
+    }
+
+    @Test
+    @DisplayName("렌트가 불가능한 상태 여부 테스트")
+    void testRentAlready() {
+        assertThat(BookRentStatus.RENT_ALREADY.isRent()).isFalse();
+    }
+}
