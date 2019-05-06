0e319bc2c0032a77c89a4f9be1e2ba2835199e56
kjc35900 <kjc3590@gmail.com>
2019-05-06 20:48:55 +0900

    feat(Hunter) create hunter service

--- src/main/java/io/github/wotjd243/findbyhint/hunter/domain/Hunter.java
+++ src/main/java/io/github/wotjd243/findbyhint/hunter/domain/Hunter.java
@@ -2,9 +2,8 @@ package io.github.wotjd243.findbyhint.hunter.domain;
 
 public class Hunter {
 
-    // TODO (1)‘헌터’는 하루에 미션 하나를 풀 수 있고 기회는 3번이다.
-    // TODO (2)‘헌터' 는 미션 포인트를 100점 모으면 문제를 풀 수 있는 총알을 하나 얻는다.
     // TODO (3)‘헌터' 가 미션을 풀 수있는 기회의 수는 총알이다. 관련된 객체 생성하기.
+    // TODO (2)‘헌터' 는 미션 포인트를 100점 모으면 문제를 풀 수 있는 총알을 하나 얻는다.
 
     private final HunterId hunterId;
 
@@ -18,13 +17,40 @@ public class Hunter {
 
     private final HunterPoint hunterPoint;
 
-    Hunter(String hunterId, String hunterPw, String hunterName, String hunterPicturePath, String hunterPictureName, int hunterPoint) {
+    private int hunterBullet;
+
+    public Hunter(String hunterId, String hunterPw, String hunterName, String hunterPicturePath, String hunterPictureName, int hunterPoint, int hunterBullet) {
         this.hunterId = new HunterId(hunterId);
         this.hunterPw = new HunterPw(hunterPw);
         this.hunterName = new HunterName(hunterName);
         this.hunterPicturePath = hunterPicturePath;
         this.hunterPictureName = new HunterPictureName(hunterPictureName);
         this.hunterPoint = new HunterPoint(hunterPoint);
+        this.hunterBullet = hunterBullet;
+    }
+
+    public void decreaseOneBullet() {
+        if (hunterBullet < 1) {
+            throw new IllegalStateException();
+        }
+        hunterBullet--;
+    }
+
+    public void increaseOneBullet() {
+        if (hunterBullet >= 3) {
+            throw new IllegalStateException();
+        }
+        hunterBullet++;
+    }
+
+    public void buyOneBullet() {
+
+        if (hunterPoint.hunterPointCheck()) {
+            hunterPoint.hunterPointMinus(100);
+            increaseOneBullet();
+        }else{
+            throw new IllegalStateException();
+        }
     }
 
 }
---
+++ src/main/java/io/github/wotjd243/findbyhint/hunter/ui/HunterController.java
@@ -0,0 +1,10 @@
+package io.github.wotjd243.findbyhint.hunter.ui;
+
+import io.github.wotjd243.findbyhint.hunter.domain.Hunter;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HunterController {
+
+
+}
