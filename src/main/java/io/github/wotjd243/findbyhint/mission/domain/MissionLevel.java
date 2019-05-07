248712bda556fbb1681efc0d21e8b279d9416399
kjc3590 <kjc3590@fineinsight.kr>
2019-05-07 21:56:09 +0900

    fix(Mission) MissionLevel modify

--- src/main/java/io/github/wotjd243/findbyhint/mission/domain/MissionLevel.java
+++ src/main/java/io/github/wotjd243/findbyhint/mission/domain/MissionLevel.java
@@ -1,26 +1,5 @@
 package io.github.wotjd243.findbyhint.mission.domain;
 
-/*
-public class MissionLevel {
-
-    private String level;
-
-    public MissionLevel(final String level) {
-        validation(level);
-        this.level = level;
-    }
-
-    private void validation(String level) {
-        if(!(level.equals("1") || level.equals("2") || level.equals("3") || level.equals("4"))) {
-            throw new IllegalArgumentException();
-        }
-    }
-<<<<<<< HEAD
-<<<<<<< HEAD
-}
-=======
-}*/
-
 public enum MissionLevel {
     EASY(1, "BRONZE",2), MEDIUM(2, "SILVER",4), HARD(3, "GOLD",8);
 
@@ -33,19 +12,6 @@ public enum MissionLevel {
         this.levelValue = levelValue;
         this.levelName = levelName;
         this.hintCounter = hintCounter;
-=======
-}*/
-
-public enum MissionLevel {
-    EASY(1, "BRONZE"), MEDIUM(2, "SILVER"), HARD(3, "GOLD");
-
-    private final int levelValue;
-    private final String levelName;
-
-    MissionLevel(int levelValue, String levelName) {
-        this.levelValue = levelValue;
-        this.levelName = levelName;
->>>>>>> d7cff6d... fix(mission): fix mission entity
     }
 
     public int getLevelValue() {
@@ -56,10 +22,5 @@ public enum MissionLevel {
         return levelName;
     }
 
-<<<<<<< HEAD
     public int getHintCounter() { return hintCounter;}
 }
->>>>>>> afac6cd... *docs README 문서 수정
-=======
-}
->>>>>>> d7cff6d... fix(mission): fix mission entity
