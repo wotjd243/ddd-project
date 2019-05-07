23a018d558c8c2daa8c2526b96dfa7fc1d7042b4
kjc3590 <kjc3590@fineinsight.kr>
2019-05-07 12:26:00 +0900

    feat(Hunter) > naverLogin Controller

--- src/main/java/io/github/wotjd243/findbyhint/hunter/ui/HunterController.java
+++ src/main/java/io/github/wotjd243/findbyhint/hunter/ui/HunterController.java
@@ -1,10 +1,18 @@
 package io.github.wotjd243.findbyhint.hunter.ui;
 
-import io.github.wotjd243.findbyhint.hunter.domain.Hunter;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HunterController {
+
+    @GetMapping("/login")
+    public String naverLogin(Model model) {
+
+        return "/login";
+    }
+
 
 
 }
