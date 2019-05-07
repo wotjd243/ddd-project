d7cff6d070bb79c5833c49a89794a332114b2a1e
DESKTOP-O3LOHED\한소진 <ppiyo5@gmail.com>
2019-05-07 11:24:37 +0900

    fix(mission): fix mission entity

--- src/main/java/io/github/wotjd243/findbyhint/mission/domain/MissionLevel.java
+++ src/main/java/io/github/wotjd243/findbyhint/mission/domain/MissionLevel.java
@@ -1,5 +1,6 @@
 package io.github.wotjd243.findbyhint.mission.domain;
 
+/*
 public class MissionLevel {
 
     private String level;
@@ -10,8 +11,29 @@ public class MissionLevel {
     }
 
     private void validation(String level) {
-        if(!(level.equals("브론즈") || level.equals("실버") || level.equals("골드") || level.equals("플래티넘"))) {
+        if(!(level.equals("1") || level.equals("2") || level.equals("3") || level.equals("4"))) {
             throw new IllegalArgumentException();
         }
     }
+}*/
+
+public enum MissionLevel {
+    EASY(1, "BRONZE"), MEDIUM(2, "SILVER"), HARD(3, "GOLD");
+
+    private final int levelValue;
+    private final String levelName;
+
+    MissionLevel(int levelValue, String levelName) {
+        this.levelValue = levelValue;
+        this.levelName = levelName;
+    }
+
+    public int getLevelValue() {
+        return levelValue;
+    }
+
+    public String getLevelName() {
+        return levelName;
+    }
+
 }
