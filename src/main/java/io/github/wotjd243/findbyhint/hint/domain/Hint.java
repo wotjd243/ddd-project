d719895762a02e65c5e8f3ca35c989558fdba05b
92kimyoungchan <naddana1@naver.com>
2019-05-07 17:16:10 +0900

    add(Hint)

--- src/main/java/io/github/wotjd243/findbyhint/hint/domain/Hint.java
+++ src/main/java/io/github/wotjd243/findbyhint/hint/domain/Hint.java
@@ -1,40 +1,40 @@
 package io.github.wotjd243.findbyhint.hint.domain;
+//TODO 1. 힌트 에그리거트는 헌터ID, 보물ID, 좌표값, 순서로 이루어져 있다.
+//TODO 2. Treasure에 해당하는 Tartgetpoint를 가져오는 메서드  ( HintService에서 Delete 메서드 실행시 보물의 좌표를 제외하고 지워야함 )
 
-import io.github.wotjd243.findbyhint.mission.domain.MissionLevel;
+
+
+
+import io.github.wotjd243.findbyhint.hunter.domain.HunterId;
+import io.github.wotjd243.findbyhint.treasure.domain.TargetPoint;
+
+import java.util.List;
 
 public class Hint {
+    // 힌트 엔티티의 PK
+    private Long HintId;
+    // FK
+    private final HunterId hunterId;
+    // FK 나중에 주입받는다 .(referenced Column ID)
+    private final Long treasureId;
+    // targetPointList 에서 추출할 값은 latitude , hardness , distinguish , hintOrder => (TargetPoint 의 PK값)
 
 
-    private HintId number;
-    private MissionLevel level;
-    private FakeTargetPoint fakeTargetPoint;
+    private final List<TargetPoint> targetPointList;
 
 
-    public Hint(final int number, final String level, final String latitude,final String hardness) {
-        this.number = HintId.valueOf(number);
-        this.level = MissionLevel.valueOfmissonLevel(level);
-        this.fakeTargetPoint= FakeTargetPoint.valueOffakeTargetPoint(latitude , hardness);
+
+    public Hint(String hunterId, Long treasureId, List<TargetPoint> targetPointList) {
+        this.hunterId = new HunterId(hunterId);
+        this.treasureId = treasureId;
+        this.targetPointList = targetPointList;
     }
 
-
-
-    public static Hint valueOf(int number, String level, String latitude, final String hardness){
-        return new Hint(number,level,latitude,hardness);
+    public static Hint valueOf(String hunterId, Long treasureId, List<TargetPoint> targetPointList) {
+        return new Hint(hunterId, treasureId, targetPointList);
     }
-
-
-    public String HintInfo() {
-        return "Hint{" +
-                "number=" + number +
-                ", level='" + level + '\'' +
-                ", targetPoint=" + fakeTargetPoint.getFakeTargetPoint().get("latitude") + fakeTargetPoint.getFakeTargetPoint().get("hardness") + '\'' +
-                '}';
-    }
-
-
 
 
 
 
 }
-
--- src/test/java/io/github/wotjd243/findbyhint/hint/domain/HintTest.java
+++ src/test/java/io/github/wotjd243/findbyhint/hint/domain/HintTest.java
@@ -1,21 +1,35 @@
 package io.github.wotjd243.findbyhint.hint.domain;
-
+import io.github.wotjd243.findbyhint.treasure.domain.TargetPoint;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HintTest {
-
-
-
     @Test
     public void 힌트생성(){
+        // given
+        final Long HintId = 4L;
+        final String HunterId = "ylips";
+        final Long treasureId = 3L;
+        final List<TargetPoint> targetPointList = new ArrayList<>();
 
-        final String level = "브론즈";
-        final String latitude = "126.9941658";
-        final String hardness = "37.4954676";
+        TargetPoint realTargetPoint= TargetPoint.valueOfIatitudeAndHardness(34.323,125.3213,"3");
 
-        Hint result = Hint.valueOf(1, level,latitude,hardness);
+        targetPointList.add(realTargetPoint);
+        for(int i=0; i<4; i++){
+            TargetPoint fakeTargetPoint = realTargetPoint.getFakeTargetPoint();
+            targetPointList.add(fakeTargetPoint);
+        }
+
+
+
+
+        // when
+        Hint result = Hint.valueOf(HunterId,treasureId,targetPointList);
+
+        // then
     }
-
-
 
 
 
