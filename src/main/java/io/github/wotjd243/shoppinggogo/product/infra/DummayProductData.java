f74d0795cc7dd278e1b834602d9d0bf999625db7
kdy0573 <kdy0573@daum.net>
2019-05-09 02:19:56 +0900

    fix(PriceRecord and DummyData)

--- src/main/java/io/github/wotjd243/shoppinggogo/product/infra/DummayProductData.java
+++ src/main/java/io/github/wotjd243/shoppinggogo/product/infra/DummayProductData.java
@@ -11,22 +11,20 @@ public class DummayProductData {
     private static final Map<Long, Product> products = new HashMap<>();
 
     static {
-        List<PriceRecord> price1s = new ArrayList<>();
-        price1s.add(new PriceRecord(1L, 10, 30, 20,
-                new Date(), new Date()));
-        price1s.add(new PriceRecord(2L,11, 31, 21, new Date(), new Date()));
-        price1s.add(new PriceRecord(3L, 12, 32, 22, new Date(), new Date()));
+        PriceRecord price1 = new PriceRecord(1L, 10, 30, 20,
+                new Date(), new Date());
         put(    1L, "DDD 프로젝트",  new Category("강의", "프로그래밍"),
                 new ProductImage(1000L, 1L, "/ddd/project/link", "file.jpg", new Date()),
-                price1s,
+                price1,
                 new Date(), new Date());
 
         List<PriceRecord> price2s = new ArrayList<>();
         price2s.add(new PriceRecord(1L, 100, 300, 200,
                 new Date(), new Date()));
+        PriceRecord price2 = new PriceRecord(2L,11, 31, 21, new Date(), new Date());
         put(    2L, "포켓몬스터",  new Category("강의", "프로그래밍"),
                 new ProductImage(2000L, 2L, "/pocket", "monster.jpg", new Date()),
-                price2s,
+                price2,
                 new Date(), new Date());
     }
 
@@ -37,7 +35,7 @@ public class DummayProductData {
     public static Product put(final long id, final String name,
                               final Category category,
                               final ProductImage productImage,
-                              final List<PriceRecord> prices,
+                              final PriceRecord prices,
                               final Date createDate, final Date updateDate) {
         return products.put(id, new Product(id, name, category, productImage,
                 prices, createDate, updateDate));
--- src/main/java/io/github/wotjd243/shoppinggogo/product/infra/DummyProductRepository.java
+++ src/main/java/io/github/wotjd243/shoppinggogo/product/infra/DummyProductRepository.java
@@ -1,8 +1,15 @@
 package io.github.wotjd243.shoppinggogo.product.infra;
 
+import io.github.wotjd243.shoppinggogo.product.domain.Product;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public class DummyProductRepository implements ProductRepository {
 
+    @Override
+    public Product findbyId(long productId) {
+        return Optional.ofNullable(DummayProductData.get(productId));
+    }
 }
