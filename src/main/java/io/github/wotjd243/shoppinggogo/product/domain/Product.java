a5fd5a641c92b49c6e95d7fe9da62d362f81a356
kdy0573 <kdy0573@daum.net>
2019-05-09 02:18:23 +0900

    fix(PriceRecord)

--- src/main/java/io/github/wotjd243/shoppinggogo/product/domain/Product.java
+++ src/main/java/io/github/wotjd243/shoppinggogo/product/domain/Product.java
@@ -15,35 +15,31 @@ public class Product {
     private String name; //상품 명
     private Category category; //상품 카테고리
     private ProductImage productImage; //제품 이미지
-    private List<PriceRecord> prices;
+    private PriceRecord prices;
     private Date createDate; // 생성 날짜
     private Date updateDate; //수정된 날짜
 
     public Product(Long id, String name, Category category, ProductImage productImage,
-           List<PriceRecord> prices, Date createDate, Date updateDate) {
+           PriceRecord prices, Date createDate, Date updateDate) {
         this.id = id;
         this.name = name;
         this.category = category;
         this.productImage = productImage;
         this.prices = prices;
-        this.createDate = createDate;
-        this.updateDate = updateDate;
     }
 
     /**
      * 제품 가장 낮은 가격을 조회한다.
      * @return
      */
-    public PriceRecord findLowestPrice() {
-        return prices.stream().min(Comparator.comparing(PriceRecord::getLowestPrice))
-                .orElseThrow(NoSuchElementException::new);
+    public int findLowestPrice() {
+        return prices.getLowestPrice();
     }
+
 
     public Long getId() {
         return id;
     }
 
-    public List<PriceRecord> getPrices() {
-        return prices;
-    }
+
 }
--- src/main/java/io/github/wotjd243/shoppinggogo/product/infra/ProductRepository.java
+++ src/main/java/io/github/wotjd243/shoppinggogo/product/infra/ProductRepository.java
@@ -1,7 +1,9 @@
 package io.github.wotjd243.shoppinggogo.product.infra;
 
 
+import io.github.wotjd243.shoppinggogo.product.domain.Product;
 
 public interface ProductRepository {
 
+    Product findbyId(long productId);
 }
