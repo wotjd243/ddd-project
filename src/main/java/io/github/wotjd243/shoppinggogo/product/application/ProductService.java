4099aab85be077266af12e62d1b36805f254029e
yanghun0070 <yanghun0070@gmail.com>
2019-05-12 14:32:29 +0900

    패키지로 인한 에러

--- src/main/java/io/github/wotjd243/shoppinggogo/product/application/ProductService.java
+++ src/main/java/io/github/wotjd243/shoppinggogo/product/application/ProductService.java
@@ -2,7 +2,6 @@ package io.github.wotjd243.shoppinggogo.product.application;
 
 import io.github.wotjd243.shoppinggogo.product.domain.Category;
 import io.github.wotjd243.shoppinggogo.product.domain.Product;
-import io.github.wotjd243.shoppinggogo.product.infra.ProductRepository;
 import jdk.nashorn.internal.runtime.options.Option;
 import org.springframework.beans.factory.annotation.Autowired;
 import io.github.wotjd243.shoppinggogo.product.domain.ProductRepository;
