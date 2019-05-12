b056ff748e29a9785790590c8518aacb14750626
tramyu <tramyu@naver.com>
2019-05-12 16:36:27 +0900

    feat(ui) : book rest controller
    - controller test base 클래스 추가
    - hateoas 의존성 추가

---
+++ book-api/src/test/java/io/github/book/common/ControllerTestBase.java
@@ -0,0 +1,21 @@
+package io.github.book.common;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+@Disabled
+public class ControllerTestBase {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    protected ObjectMapper objectMapper;
+}
