3237ed92c72eb4f498a09374bd081f223e1632ba
kdy0573 <kdy0573@daum.net>
2019-05-12 13:56:06 +0900

    OrderService Repository 어노테이션 추가

--- src/main/java/io/github/wotjd243/shoppinggogo/order/infra/DummyOrderRepository.java
+++ src/main/java/io/github/wotjd243/shoppinggogo/order/infra/DummyOrderRepository.java
@@ -2,11 +2,12 @@ package io.github.wotjd243.shoppinggogo.order.infra;
 
 import io.github.wotjd243.shoppinggogo.order.domain.Order;
 import io.github.wotjd243.shoppinggogo.order.domain.OrderRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 
-
+@Repository
 public class DummyOrderRepository implements OrderRepository {
     @Override
     public java.util.Optional<Order> findbyId(long orderId) {
