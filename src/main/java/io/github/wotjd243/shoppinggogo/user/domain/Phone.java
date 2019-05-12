b975cf05accde71c1fd67787f50691ee47b02c53
kdy0573 <kdy0573@daum.net>
2019-05-12 21:18:04 +0900

    fix(phone, email)
    	phone, email  패턴 매치 complie 위치 변경

--- src/main/java/io/github/wotjd243/shoppinggogo/user/domain/Phone.java
+++ src/main/java/io/github/wotjd243/shoppinggogo/user/domain/Phone.java
@@ -2,20 +2,26 @@ package io.github.wotjd243.shoppinggogo.user.domain;
 
 import lombok.Getter;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 @Getter
 public class Phone {
 
     private String phoneNumber;
-    private String phoneRegex  = "[0-9]{11}";
+    final static private String phoneRegex  = "[0-9]{11}";
+    final static private Pattern p = Pattern.compile(phoneRegex);
+
     public Phone(String phoneNumber) {
         validate(phoneNumber);
         this.phoneNumber = phoneNumber;
     }
 
     private void validate(String phoneNumber){
-        boolean validation = Pattern.matches(phoneRegex,phoneNumber);
+        Matcher m = p.matcher(phoneNumber);
+        boolean validation =  m.matches();
         if( validation==false) throw new IllegalArgumentException();
 
     }
 }
+
+
