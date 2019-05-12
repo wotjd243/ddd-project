c74888ebbaf9c1d5eabded51637ece629f152e36
hue9010 <hue9010@gmail.com>
2019-05-12 19:36:50 +0900

    refactor(ui) : CollectedBookRestController
    - rename CollectedController -> CollectedBookRestController
    - using HateOasResource

--- book-api/src/test/java/io/github/book/collected/ui/CollectedBookRestControllerTest.java
+++ book-api/src/test/java/io/github/book/collected/ui/CollectedBookRestControllerTest.java
@@ -1,16 +1,15 @@
 package io.github.book.collected.ui;
 
 import io.github.book.collected.application.CollectedBookService;
+import io.github.book.collected.application.dto.CollectedBookDto;
 import io.github.book.collected.domain.BookLocation;
 import io.github.book.collected.domain.CollectedBook;
 import io.github.book.common.ControllerTestBase;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
@@ -18,28 +17,28 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-class CollectedControllerTest extends ControllerTestBase {
-    @MockBean
-    private CollectedBookService collectedBookServicel;
+class CollectedBookRestControllerTest extends ControllerTestBase {
 
     private CollectedBook collectedBook;
+    private CollectedBookDto collectedBookDto;
 
     @BeforeEach
-    public void setup() {
+    public void setUp() {
         String isbn = UUID.randomUUID().toString();
         BookLocation bookLocation = BookLocation.builder().bookshelfLocation("우리집 안방 책장 1A").storeLocation("우리집 안방").build();
         collectedBook = CollectedBook.builder().bookIsbn(isbn).bookLocation(bookLocation).build();
+        collectedBookDto = collectedBook.toDto();
     }
 
     @Test
     @DisplayName("콜렉티드 북 추가하기")
     public void saveCollectedBook() throws Exception {
         this.mockMvc.perform(post("/api/v1/collected-book")
-                .content(objectMapper.writeValueAsString(collectedBook))
+                .content(objectMapper.writeValueAsString(collectedBookDto))
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
         )
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isCreated());
     }
 
 }
